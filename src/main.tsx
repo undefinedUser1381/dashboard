515e4959ea13ed5b459cc78f19a9640192610ab2
erfan <erfanghezi5050@gmail.com>
2025-01-08 19:26:45 +0330

    add nextui provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { NextUIProvider } from "@nextui-org/react";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </BrowserRouter>
+);
