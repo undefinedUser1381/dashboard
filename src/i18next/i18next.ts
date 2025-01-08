fbf966e71031b50dd7f0db5c965ae4493239c282
erfan <erfanghezi5050@gmail.com>
2025-01-08 19:27:54 +0330

    add 18next configs

---
+++ src/i18next/i18next.ts
@@ -0,0 +1,21 @@
+// import i18n from "i18next";
+
+// const resources = {
+//   en: {
+//     translation: enTranslation,
+//   },
+//   fa: {
+//     translation: faTranslation,
+//   },
+// };
+
+
+// i18n.use(initReactI18next).init({
+//   resources,
+//   lng: savedLanguage,
+//   interpolation: {
+//     escapeValue: false,
+//   },
+// });
+
+// export default i18n;
