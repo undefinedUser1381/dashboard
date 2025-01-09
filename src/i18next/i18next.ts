054b59be29a83db79ced5d413e7e46ecc08855f7
erfan <erfanghezi5050@gmail.com>
2025-01-09 12:55:00 +0330

    add i18 configs

--- src/i18next/i18next.ts
+++ src/i18next/i18next.ts
@@ -1,21 +1,24 @@
-// import i18n from "i18next";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import faTranslation from "./fa.json"
+import enTranslation from "./en.json"
 
-// const resources = {
-//   en: {
-//     translation: enTranslation,
-//   },
-//   fa: {
-//     translation: faTranslation,
-//   },
-// };
+const resources = {
+    en: {
+        translation: enTranslation,
+    },
+    fa: {
+        translation: faTranslation,
+    },
+};
 
 
-// i18n.use(initReactI18next).init({
-//   resources,
-//   lng: savedLanguage,
-//   interpolation: {
-//     escapeValue: false,
-//   },
-// });
+i18n.use(initReactI18next).init({
+    resources,
+    lng: "fa",
+    interpolation: {
+        escapeValue: false,
+    },
+});
 
-// export default i18n;
+export default i18n;
