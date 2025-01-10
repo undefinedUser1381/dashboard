5a94c0ae2d87d802136565ede218d20669e31294
erfan <erfanghezi5050@gmail.com>
2025-01-10 12:44:09 +0330

    add hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./i18next/i18next.ts";
 import { NextUIProvider } from "@nextui-org/react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
-  </BrowserRouter>
+    <HashRouter>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </HashRouter>
 );
