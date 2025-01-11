050a474c601ba1f12d08383966deafda2bd69907
erfan <erfanghezi5050@gmail.com>
2025-01-11 21:51:11 +0330

    updated ...

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import "./i18next/i18next.ts";
 import { NextUIProvider } from "@nextui-org/react";
 import { HashRouter } from "react-router-dom";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./stores/store.ts";
 
 createRoot(document.getElementById("root")!).render(
     <HashRouter>
       <NextUIProvider>
+        <Provider store={store}>
         <App />
+        </Provider>
       </NextUIProvider>
     </HashRouter>
 );
