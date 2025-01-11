db936dcd8e4b49e0165b3274cdb0a7f3611c5ddb
erfan <erfanghezi5050@gmail.com>
2025-01-11 21:50:00 +0330

    Change the name of Dashboard comp

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,20 @@
 import { RouteObject } from "react-router-dom"
 import Comments from "./pages/Comments"
 import Loginpage from "./pages/Loginpage"
-import Dashboard from "./pages/Dashboard"
+import Dashboardpage from "./pages/Dashboardpage"
 import Messages from "./pages/Messages"
 import Tables from "./pages/Tables"
 import Tasks from "./pages/Tasks"
 import Wallet from "./pages/Wallet"
 
 const routes : RouteObject[] = [
-    { path : "/" , element : <Dashboard />},
-    { path : "/wallet" , element : <Wallet />},
-    { path : "/comments" , element : <Comments />},
-    { path : "/messages" , element : <Messages />},
-    { path : "/tables" , element : <Tables />},
-    { path : "/tasks" , element : <Tasks />},
-    { path : "/login" , element : <Loginpage />}
+    { path : "/" , element : <Dashboardpage /> },
+    { path : "wallet" , element : <Wallet />},
+    { path : "profile" , element : <Comments />},
+    { path : "messages" , element : <Messages />},
+    { path : "tables" , element : <Tables />},
+    { path : "tasks" , element : <Tasks />},
+    { path : "login" , element : <Loginpage />}
 ]
 
 export default routes
