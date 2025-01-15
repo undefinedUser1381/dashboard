03d085c123fb4e4f5e378dc060d2fb781583dea9
erfan <erfanghezi5050@gmail.com>
2025-01-15 17:52:38 +0330

    Sidebar add component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,20 +1,21 @@
-import { NavLink } from "react-router-dom";
-import sideBarLogo from "../../assets/images/sidebarlogo/logo.svg";
-import { useSelector , useDispatch} from "react-redux";
-import { RootState , AppDispatch} from "@/stores/store";
-import { closeSideBar } from "@/stores/features/sidebar";
+import sideBarLogo from "../../assets/images/sidebarlogo/logo.svg"
+import MobileSidebar from "./MobileSidebar/MobileSidebar";
+import SideBarLink from "./SideBarLink/SideBarLink";
+import { CiGrid41 } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
+import { LuListTodo } from "react-icons/lu";
+import { BiSolidWalletAlt } from "react-icons/bi";
+import { TiMessages } from "react-icons/ti";
+import { MdOutlineTableRows } from "react-icons/md";
 
 function SideBar() {
-
-  const dispatch : AppDispatch = useDispatch()
-  const isOpen = useSelector((state : RootState) => state.sidebar.isOpen)
-
   return (
-    <aside className={`w-[15rem] dark:bg-[#24303F] bg-primary hidden lg:flex transition-all z-20 fixed top-0 bottom-0 left-0 overflow-hidden h-[100vh]`}>
+    <div>
+     <aside className='w-[15rem] dark:bg-[#24303F] bg-primary hidden lg:flex transition-all z-20 fixed top-0 bottom-0 left-0 overflow-hidden h-[100vh]'>
       <div className="pt-6 pl-4 h-full pr-5">
         <div className="flex items-center gap-4 justify-between">
-          <img src={sideBarLogo} alt="" />
-          <div onClick={() => dispatch(closeSideBar())} className="cursor-pointer">
+          <img src={sideBarLogo} className="w-[10rem] h-[3rem]" alt="" />
+          <div className="cursor-pointer">
            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 block lg:hidden font-bold text-gray-500">
             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
           </svg>
@@ -22,51 +23,25 @@ function SideBar() {
         </div>
         <div className="mt-16">
           <h4 className="text-secondary font-bold text-[0.9rem]">MENU</h4>
-          <div className="flex child:rounded-lg child:text-[#CEE4EE] child:font-medium child:w-full child:transition-all child:p-3 child-hover:bg-[#333A48] flex-col gap-2 mt-3">
-            <NavLink to={"/"} className="flex items-center justify-start gap-3">
-             <svg className="fill-current" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.10322 0.956299H2.53135C1.5751 0.956299 0.787598 1.7438 0.787598 2.70005V6.27192C0.787598 7.22817 1.5751 8.01567 2.53135 8.01567H6.10322C7.05947 8.01567 7.84697 7.22817 7.84697 6.27192V2.72817C7.8751 1.7438 7.0876 0.956299 6.10322 0.956299ZM6.60947 6.30005C6.60947 6.5813 6.38447 6.8063 6.10322 6.8063H2.53135C2.2501 6.8063 2.0251 6.5813 2.0251 6.30005V2.72817C2.0251 2.44692 2.2501 2.22192 2.53135 2.22192H6.10322C6.38447 2.22192 6.60947 2.44692 6.60947 2.72817V6.30005Z" fill=""></path><path d="M15.4689 0.956299H11.8971C10.9408 0.956299 10.1533 1.7438 10.1533 2.70005V6.27192C10.1533 7.22817 10.9408 8.01567 11.8971 8.01567H15.4689C16.4252 8.01567 17.2127 7.22817 17.2127 6.27192V2.72817C17.2127 1.7438 16.4252 0.956299 15.4689 0.956299ZM15.9752 6.30005C15.9752 6.5813 15.7502 6.8063 15.4689 6.8063H11.8971C11.6158 6.8063 11.3908 6.5813 11.3908 6.30005V2.72817C11.3908 2.44692 11.6158 2.22192 11.8971 2.22192H15.4689C15.7502 2.22192 15.9752 2.44692 15.9752 2.72817V6.30005Z" fill=""></path><path d="M6.10322 9.92822H2.53135C1.5751 9.92822 0.787598 10.7157 0.787598 11.672V15.2438C0.787598 16.2001 1.5751 16.9876 2.53135 16.9876H6.10322C7.05947 16.9876 7.84697 16.2001 7.84697 15.2438V11.7001C7.8751 10.7157 7.0876 9.92822 6.10322 9.92822ZM6.60947 15.272C6.60947 15.5532 6.38447 15.7782 6.10322 15.7782H2.53135C2.2501 15.7782 2.0251 15.5532 2.0251 15.272V11.7001C2.0251 11.4188 2.2501 11.1938 2.53135 11.1938H6.10322C6.38447 11.1938 6.60947 11.4188 6.60947 11.7001V15.272Z" fill=""></path><path d="M15.4689 9.92822H11.8971C10.9408 9.92822 10.1533 10.7157 10.1533 11.672V15.2438C10.1533 16.2001 10.9408 16.9876 11.8971 16.9876H15.4689C16.4252 16.9876 17.2127 16.2001 17.2127 15.2438V11.7001C17.2127 10.7157 16.4252 9.92822 15.4689 9.92822ZM15.9752 15.272C15.9752 15.5532 15.7502 15.7782 15.4689 15.7782H11.8971C11.6158 15.7782 11.3908 15.5532 11.3908 15.272V11.7001C11.3908 11.4188 11.6158 11.1938 11.8971 11.1938H15.4689C15.7502 11.1938 15.9752 11.4188 15.9752 11.7001V15.272Z" fill=""></path></svg>            
-             <p>Dashboard</p>
-            </NavLink>
-            <NavLink to={"wallet"} className="flex items-center justify-start gap-3">
-             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
-             </svg>
-             <p>Wallet</p>
-            </NavLink>
-            <NavLink to={"tasks"} className="flex items-center justify-start gap-3">
-             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-             </svg>
-             <p>Tasks</p>
-            </NavLink>
-            <NavLink className="flex items-center justify-start gap-3" to={"messages"}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
-              </svg>
-             <p>Messages</p>
-            </NavLink>
-            <NavLink className="flex items-center justify-start gap-3" to={"profile"}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-              </svg>
-             <p>Profile</p>
-            </NavLink>
-            <NavLink className="flex items-center justify-start gap-3" to={"tables"}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0 1 12 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5" />
-              </svg>
-             <p>Tables</p>
-            </NavLink>
+          <div className="flex child:text-[0.9rem] child:rounded-lg child:text-[#DEE4EE] child:font-medium child:w-full child:transition-all child:p-3 child-hover:bg-[#333A48] flex-col gap-2 mt-3">
+           <SideBarLink title="Dashboard" path="/" className={"flex items-center justify-start gap-3"} icon={<CiGrid41 className="w-6 h-6 font-bold"/>} />
+           <SideBarLink icon={<CiUser className="w-6 h-6 font-bold"/>} path="profile" title="Profile" className={"flex items-center justify-start gap-3"}/>
+           <SideBarLink icon={<LuListTodo className="w-6 h-6 font-bold"/>} path="tasks" className={"flex items-center justify-start gap-3"} title="Tasks"/>
+           <SideBarLink icon={<BiSolidWalletAlt className="w-6 h-6 font-bold"/>} title="Wallet" path="wallet" className={"flex items-center justify-start gap-3"}/>
+           <SideBarLink className="flex items-center justify-start gap-3" path="messages" title="Messages" icon={<TiMessages className="font-bold w-6 h-6" />}/>
+           <SideBarLink icon={<MdOutlineTableRows className="font-bold w-6 h-6"/>} path="tables" title="Tables" className="flex items-center justify-start gap-3"/>
           </div>
-        </div>
-        <div className="px-3 hover:bg-red-500 hover:text-primary rounded-lg py-3 cursor-pointer transition-all pr-5 text-red-400 flex items-start gap-3 mt-10">
+         </div>
+         <div className="px-3 hover:bg-red-500 hover:text-primary rounded-lg py-3 cursor-pointer transition-all pr-5 text-red-400 flex items-start gap-3 mt-10">
            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
            </svg>
           <button className="pr-5">Logout</button>
         </div>
       </div>
-    </aside>
+     </aside>
+     <MobileSidebar />
+    </div>
   );
 }
 
