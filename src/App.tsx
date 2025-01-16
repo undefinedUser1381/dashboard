43f7090cd7593a8e2ef53ffe8fa15fca5e63e2d8
erfan <erfanghezi5050@gmail.com>
2025-01-16 13:22:04 +0330

    Add AppWrapper to this file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import routes from "./routes";
-import { useRoutes } from "react-router-dom";
-function App() {
-  const router = useRoutes(routes);
+import AppWrapper from "./AppWrapper";
 
+function App() {
   return (
-    <div className="dark">
-      {router}
+    <div>
+      <AppWrapper />
     </div>
   );
 }
