8b2d53a8945b2e85c0d58c8e120ca48a09134c10
erfan <erfanghezi5050@gmail.com>
2025-01-18 20:48:42 +0330

    Updates

--- assets/index-B6NUZc8T.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-WI9DoQ8P.js";import"./index-CK_GKNmU.js";var t=a;export{t as default};
---
+++ assets/index-C_Xy4Xwp.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-B0pzCPRH.js";import"./index-GBcAboIM.js";var t=a;export{t as default};
