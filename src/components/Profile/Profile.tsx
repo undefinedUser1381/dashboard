99b0ba8024acec1f373c5f0e79609cd1cfee71b0
erfan <erfanghezi5050@gmail.com>
2025-01-18 20:09:19 +0330

    Modified

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -11,8 +11,8 @@ function Profile() {
       <section className="w-full min-h-screen bg-[#F1F5F9] dark:bg-[#1A222C]">
        <Header />
        <div className="px-[1rem] lg:px-[6rem] xl:px-[8rem] py-[2rem] xl:py-[3rem] w-full ml-auto mt-20 lg:w-[77%] xl:w-[83%]">
-       <ProfileHeader />
-       <ProfileContent />
+        <ProfileHeader />
+        <ProfileContent />
        </div>
       </section>  
     </section>
