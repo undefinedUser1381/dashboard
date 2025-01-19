1b3afea1f2ae99cf4f6056842e8dc9e5c1699594
erfan <erfanghezi5050@gmail.com>
2025-01-19 12:46:41 +0330

    Use task reducer to control of showing component

--- src/components/Tasks/TaskBody/TaskBody.tsx
+++ src/components/Tasks/TaskBody/TaskBody.tsx
@@ -1,6 +1,12 @@
 import TodoItem from "./TodoItem/TodoItem"
+import TaskModal from "../TaskModal/TaskModal"
+import { useSelector } from "react-redux"
+import { RootState } from "@/stores/store"
 
 function TaskBody() {
+
+  const isOpenModal = useSelector((state : RootState) => state.taskModal.isOpenModal)
+
   return (
     <div className="mt-8">
       <h3 className="text-black dark:text-white font-medium text-[1.2rem]">To Do's 03</h3>
@@ -10,6 +16,9 @@ function TaskBody() {
         <TodoItem title="Todo Item" isComplete={false}/>
         <TodoItem title="Todo Item" isComplete={false}/>
       </div>
+      {
+        isOpenModal && <TaskModal />
+      }     
     </div>
   )
 }
