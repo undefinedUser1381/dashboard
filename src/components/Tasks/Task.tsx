3ba4b62b55e9c9dd49c0bab018a83643f9802246
erfan <erfanghezi5050@gmail.com>
2025-01-19 12:45:25 +0330

    Modified

--- src/components/Tasks/Task.tsx
+++ src/components/Tasks/Task.tsx
@@ -10,7 +10,7 @@ function Task() {
        <section className="w-full min-h-screen bg-[#F1F5F9] dark:bg-[#1A222C]">
          <Header />
          <div className="px-[1rem] lg:px-[6rem] xl:px-[8rem] py-[2rem] xl:py-[3rem] w-full ml-auto mt-20 lg:w-[77%] xl:w-[83%]">
-           <TasksHeader />
+           <TasksHeader/>
            <TaskBody />
          </div>
        </section>
