ddfd90badb319432510e76f1005d93e1457153d6
erfan <erfanghezi5050@gmail.com>
2025-01-19 12:47:13 +0330

    Add nextui  modal button logic

--- src/components/Tasks/TasksHeader/TasksHeader.tsx
+++ src/components/Tasks/TasksHeader/TasksHeader.tsx
@@ -1,15 +1,21 @@
 import TitleLink from "@/components/TitleLink/TitleLink";
 import {Button} from "@heroui/button";
 import { LuPlus } from "react-icons/lu";
+import { useDispatch } from "react-redux";
+import { openModal } from "@/stores/features/taskmodal"; "../../../stores/features/taskmodal.ts"
+import { AppDispatch } from "@/stores/store";
 
-function TasksHeader() {
+function TasksHeader() {  
+
+  const dispatch : AppDispatch = useDispatch()
+
   return (
     <div className="flex flex-col">
       <TitleLink title="Tasks" link={"/tasks"} />
       <div className="bg-white mt-4 p-3 child:font-medium child:text-black flex items-center justify-between child:dark:text-white shadow-lg dark:bg-[#24303F]">
         <span className="text-[1.4rem]">Tasks</span>
-        <Button className="flex font-medium rounded-none bg-[#3C50E0] py-2 p-1 w-[7.5rem] items-center gap-2 justify-center">
-          <LuPlus /> Add task
+        <Button onPress={() => dispatch(openModal())} className="flex font-medium rounded-none bg-[#3C50E0] py-2 p-1 w-[7.5rem] items-center gap-2 justify-center">
+          <LuPlus className="text-white"/> <span className="text-white">Add Task</span>
         </Button>
       </div>
     </div>
