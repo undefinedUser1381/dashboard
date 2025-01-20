129d1dc070d9fb14c6a0a3222d749f5eefe235be
erfan <erfanghezi5050@gmail.com>
2025-01-20 21:00:53 +0330

    Rename wallet page to users

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,11 +5,11 @@ import Dashboardpage from "./pages/Dashboardpage"
 import Messages from "./pages/Messages"
 import Tables from "./pages/Tables"
 import Tasks from "./pages/Tasks"
-import Wallet from "./pages/Wallet"
+import Users from "./pages/Users"
 
 const routes : RouteObject[] = [
     { path : "/" , element : <Dashboardpage /> },
-    { path : "wallet" , element : <Wallet />},
+    { path : "users" , element : <Users />},
     { path : "profile" , element : <Profilepage />},
     { path : "messages" , element : <Messages />},
     { path : "tables" , element : <Tables />},
