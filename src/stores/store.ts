01ace80696d5a570adf0f36993d2d8e33a66a1ed
erfan <erfanghezi5050@gmail.com>
2025-01-31 11:12:50 +0330

    Add store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import sidebarReducer from "./features/sidebar"
 import themeReducer from "./features/theme"
 import taskReducer from "./features/taskmodal"
+import dataReducer from "./features/data"
 
 const store = configureStore({
     reducer : {
         sidebar : sidebarReducer,
         theme : themeReducer,
-        taskModal : taskReducer
+        taskModal : taskReducer,
+        data : dataReducer
     }
 })
 
