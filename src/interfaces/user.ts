c052b2e23c8fcc1f1d2b70db646d62d168906e66
erfan <erfanghezi5050@gmail.com>
2025-01-31 11:13:10 +0330

    Add user types

---
+++ src/interfaces/user.ts
@@ -0,0 +1,12 @@
+interface User {
+    id : string,
+    row : string
+    userName : string
+    email : string
+    phone : string
+    wallet : string
+    date : string
+    role : string
+  }
+
+  export type { User }
