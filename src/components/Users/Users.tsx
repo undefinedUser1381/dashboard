db8cc5fce0274516934631722bc46a2bebb0be8e
erfan <erfanghezi5050@gmail.com>
2025-01-31 11:03:45 +0330

    Change some codes ...

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,6 +1,7 @@
 import Header from "../Header/Header"
 import SideBar from "../SideBar/SideBar"
 import TitleLink from "../TitleLink/TitleLink"
+import UserBox from "./UserBox/UserBox"
 import UserAddForm from "./UsersAddForm/UserAddForm"
 
 function Users() {
@@ -12,10 +13,13 @@ function Users() {
        <div className="px-[1rem] lg:px-[6rem] xl:px-[8rem] py-[2rem] xl:py-[3rem] w-full ml-auto mt-20 lg:w-[77%] xl:w-[83%]">
         <TitleLink link="/" title="Users"/>
         <div className="flex flex-col">
-          <UserAddForm />
-        {/* All user boxes */}
+           <UserAddForm />
+           {/* All user boxes */}
+           <div className="flex mt-5 flex-col gap-3">
+             <h3 className="text-[1.4rem] dark:text-white font-medium">All users</h3>
+             <UserBox />
+           </div>
         </div>
-
        </div>
       </section>
     </section>
