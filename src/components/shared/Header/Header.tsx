02730bf523271167a9bd829820858cc74a41bd1d
erfan <erfanghezi5050@gmail.com>
2025-02-02 21:55:42 +0330

    Change the width

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -3,7 +3,7 @@ import LeftSection from "./LeftSection/LeftSection"
 
 function Header() {
   return (
-    <nav className="flex px-4 py-4 fixed z-10 right-0 top-0 bg-white dark:bg-[#24303F] shadow-md w-full lg:w-[79%] xl:w-[82.8%] items-center justify-between">
+    <nav className="flex px-4 py-4 fixed z-10 right-0 top-0 bg-white dark:bg-[#24303F] shadow-md w-full lg:w-[79%] xl:w-[84.2%] items-center justify-between">
         <LeftSection />
         <RightSection />
     </nav>
