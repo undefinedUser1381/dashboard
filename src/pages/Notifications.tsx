59388a175ad4296e268c243eda1985ed231e5c74
erfan <erfanghezi5050@gmail.com>
2025-02-02 21:11:56 +0330

    change page to notification

---
+++ src/pages/Notifications.tsx
@@ -0,0 +1,11 @@
+import Notification from "@/components/Notification/Notification"
+
+function Notifications() {
+  return (
+    <>
+     <Notification />
+    </>
+  )
+}
+
+export default Notifications
