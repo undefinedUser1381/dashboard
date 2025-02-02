f2c4fa2c922c7c6fdda1e2412d4bcae2253b70e4
erfan <erfanghezi5050@gmail.com>
2025-02-02 21:04:32 +0330

    Update task

--- src/components/Tasks/Task.tsx
+++ src/components/Tasks/Task.tsx
@@ -1,21 +1,16 @@
-import SideBar from "../SideBar/SideBar"
-import Header from "../Header/Header"
-import TasksHeader from "./TasksHeader/TasksHeader"
-import TaskBody from "./TaskBody/TaskBody"
+import ComponentLayout from "@/layout/ComponentLayout";
+import TasksHeader from "./TasksHeader/TasksHeader";
+import TaskBody from "./TaskBody/TaskBody";
 
 function Task() {
   return (
-    <section className="w-[100%] flex items-start justify-between font-bold">
-       <SideBar />
-       <section className="w-full min-h-screen bg-[#F1F5F9] dark:bg-[#1A222C]">
-         <Header />
-         <div className="px-[1rem] lg:px-[6rem] xl:px-[8rem] py-[2rem] xl:py-[3rem] w-full ml-auto mt-20 lg:w-[77%] xl:w-[83%]">
-           <TasksHeader/>
-           <TaskBody />
-         </div>
-       </section>
-    </section>
-  )
+    <ComponentLayout>
+      <div className="px-[1rem] lg:px-[6rem] xl:px-[8rem] py-[2rem] xl:py-[3rem] w-full ml-auto mt-20 lg:w-[77%] xl:w-[83%]">
+        <TasksHeader />
+        <TaskBody />
+      </div>
+    </ComponentLayout>
+  );
 }
 
-export default Task
+export default Task;
