5806e81a217c142496e8e2c9409ba98ed2de60bc
erfan <erfanghezi5050@gmail.com>
2025-02-02 21:05:37 +0330

    Update users component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,29 +1,26 @@
-import Header from "../Header/Header"
-import SideBar from "../SideBar/SideBar"
-import TitleLink from "../TitleLink/TitleLink"
-import UserBox from "./UserBox/UserBox"
-import UserAddForm from "./UsersAddForm/UserAddForm"
+import ComponentLayout from "@/layout/ComponentLayout";
+import TitleLink from "../shared/TitleLink/TitleLink";
+import UserBox from "./UserBox/UserBox";
+import UserAddForm from "./UsersAddForm/UserAddForm";
 
 function Users() {
   return (
-    <section className="w-[100%] flex items-start justify-between font-bold">
-      <SideBar />
-      <section className="w-full min-h-screen bg-[#F1F5F9] dark:bg-[#1A222C]">
-       <Header />
-       <div className="px-[1rem] lg:px-[6rem] xl:px-[8rem] py-[2rem] xl:py-[3rem] w-full ml-auto mt-20 lg:w-[77%] xl:w-[83%]">
-        <TitleLink link="/" title="Users"/>
+    <ComponentLayout>
+      <div className="px-[1rem] lg:px-[6rem] xl:px-[8rem] py-[2rem] xl:py-[3rem] w-full ml-auto mt-20 lg:w-[77%] xl:w-[83%]">
+        <TitleLink link="/" title="Users" />
         <div className="flex flex-col">
-           <UserAddForm />
-           {/* All user boxes */}
-           <div className="flex mt-5 flex-col gap-3">
-             <h3 className="text-[1.4rem] dark:text-white font-medium">All users</h3>
-             <UserBox />
-           </div>
+          <UserAddForm />
+          {/* All user boxes */}
+          <div className="flex mt-5 flex-col gap-3">
+            <h3 className="text-[1.4rem] dark:text-white font-medium">
+              All users
+            </h3>
+            <UserBox />
+          </div>
         </div>
-       </div>
-      </section>
-    </section>
-  )
+      </div>
+    </ComponentLayout>
+  );
 }
 
-export default Users
+export default Users;
