ae24de198a50e67a3555b7470c129d30a3b2d0fe
erfan <erfanghezi5050@gmail.com>
2025-02-02 20:42:20 +0330

    Handle login

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -2,11 +2,26 @@ import routes from "./routes";
 import { useRoutes } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "./stores/store";
+import Cookies from 'js-cookie';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AppWrapper() {
   const themeMode = useSelector(
     (state: RootState) => state.theme.isDarkMode
   );
+  const token = useSelector((state : RootState) => state.auth.token)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const isToken = Cookies.get("token")
+    if(!isToken){
+       navigate("login")
+    }else{
+      navigate("/")
+    }
+  },[token])
 
   const router = useRoutes(routes);
 
