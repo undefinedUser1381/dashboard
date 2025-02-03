e9d703a6ec6b52f7c68a4b54fec55ba1f9496648
erfan <erfanghezi5050@gmail.com>
2025-02-03 22:16:35 +0330

    Update taskbody

--- src/components/Tasks/TaskBody/TaskBody.tsx
+++ src/components/Tasks/TaskBody/TaskBody.tsx
@@ -2,19 +2,35 @@ import TodoItem from "./TodoItem/TodoItem"
 import TaskModal from "../TaskModal/TaskModal"
 import { useSelector } from "react-redux"
 import { RootState } from "@/stores/store"
+import { Puff } from "react-loader-spinner"
 
 function TaskBody() {
 
   const isOpenModal = useSelector((state : RootState) => state.taskModal.isOpenModal)
+  const todos = useSelector((state : RootState) => state.tasks.tasks)
+  const loading = useSelector((state : RootState) => state.tasks.isLoad)
+
+  if(loading){
+    return( 
+     <div className="text-center mt-5 flex items-center justify-center">
+       <Puff visible={true} height="40" width="40" color="#4fa94d" ariaLabel="puff-loading" wrapperStyle={{}} wrapperClass="" />
+     </div> 
+    )
+  }
 
   return (
     <div className="mt-8">
-      <h3 className="text-black dark:text-white font-medium text-[1.2rem]">To Do's 03</h3>
+      <h3 className="text-black dark:text-white font-medium text-[1.2rem]">To Do's {todos.length}</h3>
       <div className="flex flex-col gap-3 mt-5">
-        <TodoItem title="Todo Item" isComplete={false}/>
-        <TodoItem title="Todo Item" isComplete={false}/>
-        <TodoItem title="Todo Item" isComplete={false}/>
-        <TodoItem title="Todo Item" isComplete={false}/>
+        {
+          todos.length ? (
+            todos.map(todo => (
+              <TodoItem key={todo.id} {...todo}/>
+            ))
+          ) : (
+            <div className="bg-red-500 p-3 text-white text-center">No Tasks Exists</div>
+          )
+        }
       </div>
       {
         isOpenModal && <TaskModal />
