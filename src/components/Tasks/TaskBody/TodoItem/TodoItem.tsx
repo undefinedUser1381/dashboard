60545d10cb2d4dd195b72a89ca3daac558f8f83b
erfan <erfanghezi5050@gmail.com>
2025-02-03 22:16:53 +0330

    Update todoitem

--- src/components/Tasks/TaskBody/TodoItem/TodoItem.tsx
+++ src/components/Tasks/TaskBody/TodoItem/TodoItem.tsx
@@ -1,18 +1,29 @@
 import {Button} from "@heroui/button";
+import { deleteTask } from "@/stores/features/tasks";
+import { AppDispatch } from "@/stores/store";
+import { useDispatch } from "react-redux";
 
 interface TaskProps {
+  id : string
   title : string
-  isComplete : boolean
-}
+  isComplete : string
+};
 
-function TodoItem({title} : TaskProps) {
+function TodoItem({id,title , isComplete} : TaskProps) {
+
+  const dispatch : AppDispatch = useDispatch() 
+
+  const deleteTodo = (ID) => {
+      dispatch(deleteTask(ID));
+  }
+
   return (
     <div className="w-full p-5 bg-white shadow-lg dark:bg-[#24303F]">
        <div className="flex flex-col gap-4 sm:flex-row items-start sm:items-center sm:gap-0 justify-between w-full">
-           <h3 className="font-medium w-[5rem] text-gray-500 text-[0.9rem]">{title}</h3>
+           <h3 className={`font-medium ${isComplete === "true" ? "line-through text-gray-600" : "text-gray-400"} w-[8rem] text-[0.9rem]`}>{title}</h3>
            <div className="flex w-full gap-3 sm:gap-2 items-center sm justify-center sm:justify-end">
               <Button className="bg-blue-600 w-1/3 sm:w-auto text-white border-none rounded-none font-normal">Complete</Button>
-              <Button className="bg-red-600 w-1/3 sm:w-auto text-white border-none rounded-none font-normal">Delete</Button>
+              <Button onPress={() => deleteTodo(id)} className="bg-red-600 w-1/3 sm:w-auto text-white border-none rounded-none font-normal">Delete</Button>
            </div>
        </div>
     </div>
