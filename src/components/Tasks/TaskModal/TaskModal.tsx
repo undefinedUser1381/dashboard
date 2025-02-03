3566d0aacaedb891e2761f29aa74df94e6cc3602
erfan <erfanghezi5050@gmail.com>
2025-02-03 22:17:13 +0330

    Update task modal

--- src/components/Tasks/TaskModal/TaskModal.tsx
+++ src/components/Tasks/TaskModal/TaskModal.tsx
@@ -9,11 +9,19 @@ import { useSelector , useDispatch } from "react-redux"
 import { RootState , AppDispatch} from "@/stores/store"
 import { Button } from "@heroui/button";
 import { closeModal } from "@/stores/features/taskmodal";
+import { addTask } from "@/stores/features/tasks";
+import { ChangeEvent, useEffect, useState } from "react";
 
 function TaskModal() {
 
+  const [taksTitle , setTaskTitle] = useState<string>("");
   const isOpenModal = useSelector((state : RootState) => state.taskModal.isOpenModal)
   const dispatch : AppDispatch = useDispatch()
+
+  const addTodo = (title) => {
+     dispatch(addTask(title))
+     dispatch(closeModal())
+  }
 
   return (
     <Modal isOpen={isOpenModal} className="bg-[#24303F] text-white" size={"xl"}>
@@ -21,14 +29,14 @@ function TaskModal() {
           <ModalHeader onClick={() => dispatch(closeModal())} className="flex flex-col gap-1">Add Task</ModalHeader>
           <ModalBody>
             <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-              <input className="w-full p-2 outline-none border focus:border-blue-500 transition-all bg-transparent" placeholder="Task ..." type="text" />
+              <input onChange={(e : ChangeEvent<HTMLInputElement>) => setTaskTitle(e.target.value)} className="w-full p-2 outline-none border focus:border-blue-500 transition-all bg-transparent" placeholder="Task ..." type="text" />
             </div>
           </ModalBody>
           <ModalFooter>
             <Button onPress={() => dispatch(closeModal())} className="rounded-none bg-red-600 text-white">
               Close
             </Button>
-            <Button className="rounded-none bg-blue-600 text-white">
+            <Button onPress={() => addTodo(taksTitle)} className="rounded-none bg-blue-600 text-white">
               Add
             </Button>
           </ModalFooter>
