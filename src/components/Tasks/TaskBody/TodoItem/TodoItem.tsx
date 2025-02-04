2b7e7ee4996a5d4385d0fa7f7ffbdddfb6ac0426
erfan <erfanghezi5050@gmail.com>
2025-02-04 20:42:34 +0330

    Add update logic in this component

--- src/components/Tasks/TaskBody/TodoItem/TodoItem.tsx
+++ src/components/Tasks/TaskBody/TodoItem/TodoItem.tsx
@@ -1,29 +1,33 @@
 import {Button} from "@heroui/button";
-import { deleteTask } from "@/stores/features/tasks";
+import { deleteTask, doTask } from "@/stores/features/tasks";
 import { AppDispatch } from "@/stores/store";
 import { useDispatch } from "react-redux";
 
 interface TaskProps {
   id : string
   title : string
-  isComplete : string
+  isComplete : boolean
 };
 
 function TodoItem({id,title , isComplete} : TaskProps) {
-
+  
   const dispatch : AppDispatch = useDispatch() 
 
   const deleteTodo = (ID) => {
-      dispatch(deleteTask(ID));
+    dispatch(deleteTask(ID));
+  }
+
+  const updateTodo = () => {
+    dispatch(doTask({id , isComplete : !isComplete , title}))
   }
 
   return (
     <div className="w-full p-5 bg-white shadow-lg dark:bg-[#24303F]">
        <div className="flex flex-col gap-4 sm:flex-row items-start sm:items-center sm:gap-0 justify-between w-full">
-           <h3 className={`font-medium ${isComplete === "true" ? "line-through text-gray-600" : "text-gray-400"} w-[8rem] text-[0.9rem]`}>{title}</h3>
+           <h3 className={`font-medium ${isComplete ? "line-through text-gray-500" : "text-gray-300"} w-[8rem] text-[0.9rem]`}>{title}</h3>
            <div className="flex w-full gap-3 sm:gap-2 items-center sm justify-center sm:justify-end">
-              <Button className="bg-blue-600 w-1/3 sm:w-auto text-white border-none rounded-none font-normal">Complete</Button>
-              <Button onPress={() => deleteTodo(id)} className="bg-red-600 w-1/3 sm:w-auto text-white border-none rounded-none font-normal">Delete</Button>
+              <Button onPress={updateTodo} className="bg-blue-600 w-1/3 sm:w-[6rem] text-white border-none rounded-none font-normal">{isComplete ? "Incomplete" : "Complete"}</Button>
+              <Button onPress={() => deleteTodo(id)} className="bg-red-600 w-1/3 sm:w-[6rem] text-white border-none rounded-none font-normal">Delete</Button>
            </div>
        </div>
     </div>
