905e47ef8f1d1008ff472fa1be0dff5ac56fb0d5
erfan <erfanghezi5050@gmail.com>
2025-02-06 17:33:27 +0330

    Add user form modal

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,16 +1,38 @@
 import ComponentLayout from "@/layout/ComponentLayout";
 import TitleLink from "../shared/TitleLink/TitleLink";
 import UserBox from "./UserBox/UserBox";
-import UserAddForm from "./UsersAddForm/UserAddForm";
+import { useForm } from "react-hook-form";
+import AddForm from "../shared/AddForm/AddForm";
+import { AddUser } from "@/interfaces/add-user"; 
+import { SubmitHandler } from "react-hook-form";
+import { RiUserAddLine } from "react-icons/ri";
+import { useState } from "react";
 
 function Users() {
 
+  const [openFormModal , setisOpenFormModal] = useState<boolean>(true)
+
+  const closeForm = () => {
+    setisOpenFormModal(false)
+  }
+
+  const openForm = () => {
+    setisOpenFormModal(true)
+}
+
+const { handleSubmit , register , formState : { errors } } = useForm()
+
+const submitUser : SubmitHandler<AddUser> = (data) => {
+    console.log(data);
+}
+
   return (
-    <ComponentLayout>
-      <div className="px-[1rem] lg:px-[6rem] xl:px-[8rem] py-[2rem] xl:py-[3rem] w-full ml-auto mt-20 lg:w-[77%] xl:w-[83%]">
-        <TitleLink link="/" title="Users" />
-        <div className="flex flex-col">
-          <UserAddForm />
+    <>
+     <ComponentLayout>
+       <div className="px-[1rem] lg:px-[6rem] xl:px-[8rem] py-[2rem] xl:py-[3rem] w-full ml-auto mt-20 lg:w-[77%] xl:w-[83%]">
+         <TitleLink link="/" title="Users" />
+         <div className="flex flex-col">
+       
           {/* All user boxes */}
           <div className="flex mt-5 flex-col gap-3">
             <h3 className="text-[1.4rem] dark:text-white font-medium">
@@ -18,9 +40,35 @@ function Users() {
             </h3>
             <UserBox/>
           </div>
-        </div>
-      </div>
-    </ComponentLayout>
+         </div>
+       </div>
+       {
+         openFormModal && 
+         <AddForm onClose={closeForm} onSubmit={handleSubmit(submitUser)} title="Add User Form">
+          <input type="text" name="name" {...register("name",{ minLength : { value : 3 , message : "Must be more than 3" } , maxLength : {value : 20 , message : "Must be less than 20"} , required : "This Filed Is Required"})} className="w-full font-medium p-2 rounded-lg transition-all border border-gray-500 focus:border-blue-500 outline-none text-white" placeholder="Name ..."/>
+          {errors.name && <span className="w-full bg-red-400 text-white font-medium rounded-md p-1">{errors.name.message}</span>}
+          <input type="text" name="email" {...register("email",{ minLength : { value : 3 , message : "Must be more than 3" } , maxLength : {value : 20 , message : "Must be less than 20"} , required : "This Filed Is Required"})} className="w-full font-medium p-2 rounded-lg transition-all border border-gray-500 focus:border-blue-500 outline-none text-white" placeholder="Email ..."/>
+          {errors.email && <span className="w-full bg-red-400 text-white font-medium rounded-md p-1">{errors.email.message}</span>}
+          <input type="text" name="phone" {...register("phone",{minLength : { value : 3 , message : "Must be more than 3" } , maxLength : {value : 20 , message : "Must be less than 20"} , required : "This Filed Is Required"})} className="w-full font-medium p-2 rounded-lg transition-all border border-gray-500 focus:border-blue-500 outline-none text-white" placeholder="Phone ..." />
+          {errors.phone && <span className="w-full bg-red-400 text-white font-medium rounded-md p-1">{errors.phone.message}</span>}
+          <div className="flex flex-col gap-2 w-full">
+           <label className="dark:text-white text-black font-medium text-[0.8rem]" htmlFor="">Choose Role</label>
+           <select name="role" {...register("role" , { required : "Required" })} className="w-[100%] border-gray-500 dark:text-white text-black border focus:border-blue-400 transition-all font-medium p-2" id="">
+           <option value={"USER"}>User</option>
+           <option value={"ADMIN"}>Admin</option>
+          </select>
+          {errors.role && <span className="w-full bg-red-400 text-white">{errors.role.message}</span>}
+         </div>
+         <div className="w-full flex items-center justify-center md:justify-end mt-2">
+           <button className="bg-green-500 flex items-center justify-center gap-2 p-2 font-medium text-white w-[100%] rounded-lg md:w-[40%] lg:w-[20%]">
+            <RiUserAddLine />
+            Add User
+           </button>
+         </div>
+       </AddForm>
+       }
+     </ComponentLayout>
+    </>
   );
 }
 
