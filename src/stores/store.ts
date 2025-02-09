f5bff4c24cc1ea40e91fda462634e8535dccb753
erfan <erfanghezi5050@gmail.com>
2025-02-09 16:09:02 +0330

    Store updated

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -5,6 +5,7 @@ import taskReducer from "./features/taskmodal"
 import authReducer from "./features/auth"
 import tasksReducer from "./features/tasks"
 import userReducer from "./features/users"
+import articleReducer from "./features/articles"
 
 const store = configureStore({
     reducer : {
@@ -13,7 +14,8 @@ const store = configureStore({
         taskModal : taskReducer,
         auth : authReducer,
         tasks : tasksReducer,
-        users : userReducer
+        users : userReducer,
+        articles : articleReducer
     }
 })
 
