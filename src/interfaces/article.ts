42c58a3534e38991322970bb800dbe5486e8af45
erfan <erfanghezi5050@gmail.com>
2025-02-09 16:09:26 +0330

    Artilce type created

---
+++ src/interfaces/article.ts
@@ -0,0 +1,11 @@
+interface ArticleType {
+    id : string
+    title : string
+    imgSrc : string 
+    description : string
+    category : string
+    author : string
+    duration : string
+}
+
+export type { ArticleType };
